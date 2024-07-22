472c615c63941ad68da40fc92384ab25d7b81c6d
森裕輔 <moriyuusuke@moriyuusukenoMacBook-Pro-2.local>
2024-07-22 14:01:45 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/pin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,18 @@
+import Cocoa
+import FlutterMacOS
+
+class MainFlutterWindow: NSWindow {
+  override func awakeFromNib() {
+    let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    super.awakeFromNib()
+  }
+}
+func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+}
